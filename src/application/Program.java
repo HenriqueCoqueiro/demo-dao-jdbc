f7845c5254ecea9e88eda2aa95fb2c25a4badc1a
HenriqueCoqueiro <henriquecoqueiromelo@gmail.com>
2024-07-22 20:32:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Henrique", "Henrique@gmail.com", new Date(), 2000.00, department);
 
-        System.out.println(department);
+        System.out.println(seller);
     }
 }
