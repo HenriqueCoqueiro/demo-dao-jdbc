c9c2267b8e005ab230298076626cc6c7c089b6ac
HenriqueCoqueiro <henriquecoqueiromelo@gmail.com>
2024-07-25 13:13:29 -0300

    Reusing instanciation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,18 +2,16 @@ package application;
 
 import model.dao.SellerDao;
 import model.entities.DaoFactory;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(21, "Henrique", "Henrique@gmail.com", new Date(), 2000.00, department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
 
         System.out.println(seller);
     }
--- src/model/entities/DaoFactory.java
+++ src/model/entities/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.entities;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDaoJDBC createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
